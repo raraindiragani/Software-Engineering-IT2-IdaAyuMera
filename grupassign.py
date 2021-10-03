7f8f92ade23cfbf7cbb181ddf69f3b01cec998d3
raraindiragani <75958884+raraindiragani@users.noreply.github.com>
2021-10-03 21:29:58 +0700

    Group with Puan

---
+++ grupassign.py
@@ -0,0 +1,8 @@
+# Puan and Mera Assignment
+# Cek input user adalah kelipatan 3
+# Selain kelipatan 3, break
+
+while True:
+  number = int(input("input the number:"))
+  if (number % 3) != 0:
+    break
